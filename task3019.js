88ca820cc92030c85008287b477e64ca3926320b
Your Name <abdulmahroof3010@gmail.com>
2026-04-27 10:12:22 +0530

    feat:solved leetcode 3019

---
+++ task3019.js
@@ -0,0 +1,16 @@
+function changekey(s){
+    let count=0;
+
+    for(let i=1;i<s.length;i++){
+        if(s[i-1].toLowerCase() !==s[i].toLowerCase()){
+            count ++
+        }
+    }
+    return count
+
+}
+
+console.log(changekey("aAbBcC"));
+console.log(changekey("AaAaAaaA"));
+
+
