56fd630c43042eefe09df690e3d3a0066ec307df
Your Name <abdulmahroof3010@gmail.com>
2026-08-10 12:59:43 +0530

    sloved leetcode 2011 question

---
+++ 2011.js
@@ -0,0 +1,20 @@
+function finalValueAfterOperations (operations){
+    let x=0;
+
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]==="X++" || operations[i]==="++X"){
+            x++
+        }else{
+            x--
+        }
+    }
+
+    return x
+
+
+}
+
+console.log(finalValueAfterOperations(["--X","X++","X++"]));
+console.log(finalValueAfterOperations( ["++X","++X","X++"]));
+console.log(finalValueAfterOperations(["X++","++X","--X","X--"]));
+
