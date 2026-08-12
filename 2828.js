25a55e5f7114d507d0e87cdc8fcec4969dd54da7
Your Name <abdulmahroof3010@gmail.com>
2026-08-12 09:56:42 +0530

    solved leetcode question 2828.js

---
+++ 2828.js
@@ -0,0 +1,19 @@
+function isAcronym(words,s){
+
+    if(words.length !==s.length){
+        return false
+    }
+
+     for(let i=0;i<words.length;i++){
+        if(words[i][0] !==s[i]){
+            return false
+        }
+        
+     }
+     return true
+
+}
+
+console.log(isAcronym(["alice","bob","charlie"],"abc"))
+console.log(isAcronym(["an","apple"],"a"))
+console.log(isAcronym(["never","gonna","give","up","on","you"],"ngguoy"))
